ad49edfb61bbe23ad2015bac76aed5b2dd8d198d
USER01 <USER01@LAPPAD>
2018-03-09 20:08:18 +0000

    Inicio da programacao do home controller

--- CalculadoraCompleta/CalculadoraCompleta/Controllers/HomeController.cs
+++ CalculadoraCompleta/CalculadoraCompleta/Controllers/HomeController.cs
@@ -9,8 +9,38 @@ namespace CalculadoraCompleta.Controllers
     public class HomeController : Controller
     {
         // GET: Home
+        [HttpGet] //anotacao opcional, por defeito e GET:Home
         public ActionResult Index()
         {
+            //inicializar o valor do visor
+            ViewBag.Visor = 0;
+            return View();
+        }
+
+        // POST: Home
+        [HttpPost]
+        public ActionResult Index(string bt, string visor)
+        {
+            //identificar o valor da variavel "bt"
+            switch (bt)
+            {
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                    if (visor.Equals("0")) visor = bt;
+                    else visor += bt;
+                    break;
+            }
+            //enviar resposta para cliente
+            ViewBag.Visor = visor;
+
+
             return View();
         }
     }
