0dee841fdc949637c9dc308502be748b0b8865b9
MainLP <rjjordao1991@hotmail.com>
2018-03-16 05:29:17 +0000

    Programacao da virgula, clear, 0, +/-. E as suas regras. Tentativa falhada de calcular.

--- CalculadoraCompleta/CalculadoraCompleta/Controllers/HomeController.cs
+++ CalculadoraCompleta/CalculadoraCompleta/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace CalculadoraCompleta.Controllers
 {
     public class HomeController : Controller
     {
+
         // GET: Home
         [HttpGet] //anotacao opcional, por defeito e GET:Home
         public ActionResult Index()
@@ -17,10 +18,14 @@ namespace CalculadoraCompleta.Controllers
             return View();
         }
 
+        int operandoaux = 0;
+        int operandoactual = 0;
+        string operador = "";
         // POST: Home
         [HttpPost]
         public ActionResult Index(string bt, string visor)
         {
+            int resultado = 0;
             //identificar o valor da variavel "bt"
             switch (bt)
             {
@@ -33,12 +38,72 @@ namespace CalculadoraCompleta.Controllers
                 case "7":
                 case "8":
                 case "9":
-                    if (visor.Equals("0")) visor = bt;
-                    else visor += bt;
+                    if (visor.Length <= 12)
+                    {
+                        if (visor.Equals("0")) visor = bt;
+                        else visor += bt;
+                    }
+                    break;
+                case "0":
+                    if (visor.Length <= 12)
+                    {
+                        if (visor.IndexOf(",") == (1)) //se tiver virgula...
+                        {
+                            visor += bt;  //verificar se contem virgula(IndexOf se nao existir ',' tem '-1' como valor, e '1' se tiver) 
+                        }
+                        else
+                        {
+                            if (visor.Equals("0")) //se o display estiver a zero...
+                            {
+                                visor = "0";
+                            }
+                            else visor += bt;           //escrever um zero no display...
+                        }
+                    }
+                    break;
+                //-------------------------------------------------------------
+                case "/":
+                case "*":
+                case "-":
+                case "+":
+                    operandoaux = Convert.ToInt32(visor);      //ao carregar num operador, guarda o valor do display numa variavel, metendo o display a zero
+                    operador = bt;                              //guardar o operador
+                    visor = "0";
+                    break;
+                case "=":
+                    operandoactual = Convert.ToInt32(visor);
+                    switch (operador)
+                    {
+                        case "+": resultado = operandoaux + operandoactual; break; //soma
+                        case "-": resultado = operandoaux - operandoactual; break; //sub
+                        case "x": resultado = operandoaux * operandoactual; break; //mult
+                        case ":": resultado = operandoaux / operandoactual; break; //div
+                    }
+                    visor = resultado.ToString();
+                    break;
+                //--------------------------------------------------------------
+                case "C":
+                    visor = "0";
+                    break;
+                case "+/-":
+                    if (visor.Length <= 12)
+                    {
+                        if (visor != ("0"))
+                        {
+                            if (visor[0].ToString() != ("-")) visor = "-" + visor;
+                            else visor = visor.Substring(1);
+                            break;
+                        }
+                    }
+                    break;
+                case ",":
+                    if (visor.IndexOf(",") == (-1))  //verificar se contem virgula(IndexOf se nao existir ',' tem '-1' como valor, e '1' se tiver)
+                        visor += ",";
                     break;
             }
             //enviar resposta para cliente
             ViewBag.Visor = visor;
+
 
 
             return View();
